a32f6cc0c7f6543be1d1f1e394552db4321741f4
chellexking <chellezxxx@gmail.com>
2019-07-31 00:54:47 +0800

    more backend functionalities

--- main.js
+++ main.js
@@ -18,17 +18,15 @@ const db = firebase.database();
 var ref = db.ref('Users/Location_Date');
 ref.on('value', gotData, errData);
 
-var place;
-
 function gotData(data){
   console.log(data.val());
   var info = data.val();
-  window.place = info.Place;
+  window.place = info.Place; //make global variable for place
   console.log(place);
   document.getElementById("Place").innerHTML= "in " + place;
 
-  var startDate = Date.parse(info.Date_Start);
-  var endDate = Date.parse(info.Date_End);
+  window.startDate = Date.parse(info.Date_Start);
+  window.endDate = Date.parse(info.Date_End);
   var dateDiff = Math.floor((endDate-startDate)/(1000*60*60*24));
   console.log(dateDiff);
   document.getElementById("Days").innerHTML=  dateDiff + " Days";
@@ -40,6 +38,44 @@ function errData(err){
 }
 
 
+
+
+//this is for tourGuide form functionalities
+$(document).ready(function() {
+	//MODALS
+	//OPEN
+	$("body").on("click", ".modal_open", function(e) {
+		e.preventDefault();
+    $(".modal_main").addClass("active");
+		$(".modal_overlay").addClass("active");
+    getDates(startDate,endDate);
+	});
+	//CLOSE
+	$("body").on("click", ".modal_overlay", function(e) {
+		e.preventDefault();
+		$(".modal_overlay").removeClass("active");
+		$(".modal_main").removeClass("active");
+	});
+});
+
+//to get all the dates in between the two dates
+function getDates(startDate, stopDate) {
+    window.dateArray = [];
+    var currentDate = moment(startDate);
+    var stopDate = moment(stopDate);
+    while (currentDate <= stopDate) {
+        dateArray.push( moment(currentDate).format('YYYY-MM-DD') )
+        currentDate = moment(currentDate).add(1, 'days');
+    }
+    console.log(dateArray);
+
+    var i;
+    var text="";
+    for (i=0; i<dateArray.length; i++){
+      text += dateArray[i] + "<br>";
+    }
+    document.getElementById("allDates").innerHTML= text;
+}
 
 //tabs functionalities
 				var waveBtn = (function () {
