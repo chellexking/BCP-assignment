3503e470bf2535925ea6cfe088d7911df2f0b9c6
chellexking <chellezxxx@gmail.com>
2019-07-31 15:42:48 +0800

    completed whereToStay and TourGuide functions

--- main.js
+++ main.js
@@ -119,38 +119,6 @@ function getDates(startDate, stopDate) {
     document.getElementById("allDates").innerHTML= text;
 }
 
-//tabs functionalities
-				var waveBtn = (function () {
-		  'use strict';
-		  var btn = document.querySelectorAll('.wave'),
-			  tab = document.querySelector('.tab-bar'),
-			  indicator = document.querySelector('.indicator'),
-			  indi = 0;
-		  indicator.style.marginLeft = indi + 'px';
-
-		  for(var i = 0; i < btn.length; i++) {
-			btn[i].onmousedown = function (e) {
-			  var newRound = document.createElement('div'),x,y;
-
-			  newRound.className = 'cercle';
-			  this.appendChild(newRound);
-
-			  x = e.pageX - this.offsetLeft;
-			  y = e.pageY - this.offsetTop;
-
-			  newRound.style.left = x + "px";
-			  newRound.style.top = y + "px";
-			  newRound.className += " anim";
-
-			  indicator.style.marginLeft = indi + (this.dataset.num-1) * 200 + 'px';
-
-			  setTimeout(function() {
-				newRound.remove();
-			  }, 1200);
-			};
-		  }
-		}());
-
     //this is for the tabs links
     function OpenAttraction(){
       window.location = "attractions.html";}
